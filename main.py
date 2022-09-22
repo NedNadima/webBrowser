e15ce177873dcdcb66656c79b9dc7a57117aa660
Ned Nadima <nnadima1@gmail.com>
2022-09-21 21:49:47 -0400

    Added google as source, plus further edit around the tabs

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from PyQt5.QtWidgets import (QApplication, QWidget,
                              QVBoxLayout, QHBoxLayout,
                              QLabel, QLineEdit,
                              QTabBar, QFrame,
-                             QStackedLayout, QTabWidget)
+                             QStackedLayout, QTabWidget, QPushButton)
 
 from PyQt5.QtGui import QIcon, QWindow, QImage
 from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
 
 class AddressBar(QLineEdit):
     def __init__(self):
@@ -21,7 +22,7 @@ class AddressBar(QLineEdit):
 class App(QFrame):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Web Browser")
+        self.setWindowTitle("Ned Browser")
         self.CreateApp()
         self.setBaseSize(1366, 768)
 
@@ -35,10 +36,12 @@ class App(QFrame):
         self.tabbar = QTabBar(movable=True, tabsClosable=True)
         self.tabbar.tabCloseRequested.connect(self.CloseTab)
 
-        self.tabbar.addTab("Tab 1")
-        self.tabbar.addTab("Tab 2")
 
         self.tabbar.setCurrentIndex(0)
+
+        #Keep track of tabs
+        self.tabCount = 0
+        self.tabs = []
 
         #Create AddressBar
         self.Toolbar = QWidget()
@@ -48,15 +51,56 @@ class App(QFrame):
         self.Toolbar.setLayout(self.ToolbarLayout)
         self.ToolbarLayout.addWidget(self.addressbar)
 
+        #New tab button
+        self.AddTabButton = QPushButton("+")
+        self.AddTabButton.clicked.connect(self.AddTab)
+
+        self.ToolbarLayout.addWidget(self.AddTabButton)
+
+        #set main view
+        self.container = QWidget()
+        self.container.layout = QStackedLayout()
+        self.container.setLayout(self.container.layout)
+
         self.layout.addWidget(self.tabbar)
         self.layout.addWidget(self.Toolbar)
+        self.layout.addWidget(self.container)
 
         self.setLayout(self.layout)
 
+        self.AddTab()
         self.show()
 
     def CloseTab(self, i):
         self.tabbar.removeTab(i)
+
+    def AddTab(self):
+        i = self.tabCount
+
+        self.tabs.append(QWidget())
+        self.tabs[i].layout=QVBoxLayout()
+        self.tabs[i].setObjectName("tab"+ str(i))
+
+        self.tabs[i].content = QWebEngineView()
+        self.tabs[i].content.load(QUrl.fromUserInput("http://google.com"))
+
+        #Add webview to tabs layout
+        self.tabs[i].layout.addWidget(self.tabs[i].content)
+
+
+        #set top level tab from [] to layout
+        self.tabs[i].setLayout(self.tabs[i].layout)
+
+        #Add tab to top level stackewidget
+        self.container.layout.addWidget(self.tabs[i])
+        self.container.layout.setCurrentWidget(self.tabs[i])
+
+        #Set the tab at top of screen
+        self.tabbar.addTab("New Tab")
+       # self.tabbar.setTabData("tab"+ str(i))
+        self.tabbar.setCurrentIndex(i)
+
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
